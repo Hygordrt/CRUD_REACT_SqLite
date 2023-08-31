45109c0546c0c8bb1ecab7b1710a600646853e36
Hygor <Hygor@DESKTOP-55BA59J>
2023-08-31 01:12:58 -0300

    Componente grid

---
+++ front_end/src/components/Grid.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import axios from 'axios';
+
+import Styled from "styled-components";
+import { FaTrash, FaEdit } from "react-icons/fa";
+
+const Table = Styled.table`
+    width: 100%;
+    background-color: #fff;
+    padding: 20px;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 5px;
+    max-width: 800px;
+    margin: 20px auto;
+    word-break: break-all;
+`;
+
+const Thead = Styled.thead``;
+
+const Tbody = Styled.tbody``;
+
+const Tr = Styled.tr``;
+
+const Th = Styled.th`
+    text-align: start;
+    border-bottom: inset;
+    padding-bottom:  5px;
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display: none"}
+    }
+`;
+
+export const Td = Styled.td`
+    padding-top: 15px;
+    text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+    width: ${(props) => (props.width ? props.width : "auto")};
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display: none"}
+    }
+`;
+
+const Grid = ({ users }) => {
+  return (
+    <Table>
+        <Thead>
+            <Tr>
+                <Th>Nome</Th>
+                <Th>E-mail</Th>
+                <Th onlyWeb>Telefone</Th>
+            </Tr>
+        </Thead>
+        <Tbody>
+            {users.map((item, i) => (
+                <Tr key={i}>
+                    <Td width="30%">{item.nome}</Td>
+                    <Td width="30%">{item.email}</Td>
+                    <Td width="20%" onlyWeb>{item.telefone}</Td>
+                    <Td alignCenter width="5%">
+                        <FaEdit />
+                    </Td>
+                    <Td alignCenter width="5%">
+                        {/* <FaTrash onClick={() => handleDelete(item.id)}/> */}
+                        <FaTrash/>
+                    </Td>
+                </Tr>
+            ))}
+        </Tbody>
+    </Table>
+  )
+}
+
+export default Grid
