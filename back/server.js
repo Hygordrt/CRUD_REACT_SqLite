4b35005f212ced30123ae4dcfd1a1166a995b2d4
Hygor <Hygor@DESKTOP-55BA59J>
2023-08-30 23:03:49 -0300

    back criado e configurado
    Pacote 'express' para roteamento e 'sqlite3' para banco de dados

---
+++ back/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+const db = new sqlite3.Database("./db/database.sqlite");
+
+app.get('/api/data', (req, res) => {
+  const query = 'SELECT * FROM form_desafio'; 
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Erro ao buscar dados.' });
+    } else {
+      if (rows.length === 0) {
+        res.status(404).json({ message: 'Nenhum dado encontrado.' });
+      } else {
+        res.json(rows);
+      }
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor API rodando na porta ${port}`);
+});
